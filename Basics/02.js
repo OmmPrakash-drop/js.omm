1434e5ab91ecf0160a6b8f285ed15d4b3f8eaa45
Omm Prakash Satapathy <ommprakshsatapathy@gmail.com>
2025-06-06 18:52:44 +0000

    day2

---
+++ Basics/02.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all JS as newer version
+
+// alert("hello")   //wee are using node.js not browser
+
+//code readability should be good
+
+let name = "Omm"
+let age = 19
+let isLoggedIn = false
+//number => 2^53
+//bigint  (large numbers)
+//string => ""
+//bolean => true/false
+//null => empty value, stand alone value
+//undefined => variable not defined
+//symbol => unique
+//object => collection of key value pairs
+
+console.log(typeof name)  //type of the given variable
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
---
+++ Basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="ommprakshsatapthythy@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2  //not allowed
+
+accountEmail = "omm@gmail.com"
+accountPassword= "12312"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
