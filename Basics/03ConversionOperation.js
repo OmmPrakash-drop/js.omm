86e3b5322745ff1508571217f217d8081a0d813d
Omm Prakash Satapathy <ommprakshsatapathy@gmail.com>
2025-06-07 12:14:03 +0000

    conversionOf Datatype

--- Basics/03ConversionOperation.js
+++ Basics/03ConversionOperation.js
@@ -1,5 +1,30 @@
-let score = 33
+// 03 ConversionOperation
+let score = null
 
 // const {score} =req.body
 
-console.log(typeof socre);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//   "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "hii"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0=> false
+// "" => false
+// "Omm"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
