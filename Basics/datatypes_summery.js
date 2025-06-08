b4a1943ab75695f54c14802817ebdf32d6bac9e7
Omm Prakash Satapathy <ommprakshsatapathy@gmail.com>
2025-06-08 05:21:31 +0000

    memory

--- Basics/datatypes_summery.js
+++ Basics/datatypes_summery.js
@@ -30,3 +30,28 @@ const myFunction = function(){
 
 console.log(typeof bigNumber);
 console.log(typeof outsideTemp);
+
+// =============================memory========================================
+
+// two type of memory are there 
+// stack (primitive)(copy of data), heap (Non Primitive)(reference of data)
+
+let myName = "Omm"
+let anotherName = myName
+anotherName = "Raja"
+
+console.log(myName);
+console.log(anotherName);
+// here it is a primitive data type which provides a copy during referencing the data, there for the change occures in the copy not in the original data.
+let userOne ={
+    email: "omm@hii",
+    upi: "omm@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "omm@code"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+// here it is a nonprimitive data type which is heap, during referencing it basically provides the original data which will change during any change in the referenced datatype.
