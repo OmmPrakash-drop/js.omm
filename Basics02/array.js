c40c3844d348d8ca25baa4636406a137b0dfe63c
Omm Prakash Satapathy <ommprakshsatapathy@gmail.com>
2025-06-11 18:04:04 +0000

    little update

--- Basics02/array.js
+++ Basics02/array.js
@@ -20,10 +20,10 @@ console.log(myarray[1]);
 // myarray.unshift(0) //add element inside braces on front
 // myarray.shift(0) //shift/delete element from front
 
-// console.log(myarray.includes(1));
-// console.log(myarray.indexOf(3));
+// console.log(myarray.includes(1));  //is the array includes the value inside the parenthesis
+// console.log(myarray.indexOf(3));    //shows the index of the value inside the parenthesis
 
-// const newarray = myarray.join();
+// const newarray = myarray.join();  //joins all the element into a string/ change the type into string
 // console.log(newarray)
 // console.log(typeof newarray);
 
@@ -33,7 +33,7 @@ console.log(myarray[1]);
 // slice, splice
 console.log("A", myarray)
 
-const myn1= myarray.slice(1, 3)  //print the index positions only exclude last value
+const myn1= myarray.slice(1, 3)  //print the index positions only exclude last position of the given value inside the parenthesis, here 3rd position is not going to be print
 
 console.log(myn1);
 console.log("B",myarray);
