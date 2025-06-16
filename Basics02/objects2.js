807f25df532155fb9cb1f7abd14be3f4cb319b74
Omm Prakash Satapathy <ommprakshsatapathy@gmail.com>
2025-06-16 11:35:00 +0000

    object part 2 may be

--- Basics02/objects2.js
+++ Basics02/objects2.js
@@ -16,6 +16,43 @@ const regularuser = {
         }
     }
 }
-console.log(regularuser.fullname.userfullname)
+// console.log(regularuser.fullname.userfullname)
 
-const obj1 = {}
+const obj1 = {1 : "a", 2: "b"}
+const obj2 = {3 : "a", 4: "b"}
+// const obj3 = { obj1, obj2 }
+
+
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+const obj3 ={...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users =[
+    {
+        id: 1,
+        email: "omm@gmail.com"
+    },
+    {
+        id: 1,
+        email: "omm@gmail.com"
+    },
+    {
+        id: 1,
+        email: "omm@gmail.com"
+    },
+    {
+        id: 1,
+        email: "omm@gmail.com"
+    },
+]
+users[1].email
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLogedin'));
